6f1a3f90980a967f4abd5725ef89be1ec4bd5bdd
Sebastian Rojas <sebastianrojas812@gmail.com>
2023-07-19 13:06:26 -0400

    init, clerk auth provider and sign in/up, sidebar and mobile sheet component, shadcn init, dashboard page

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { ArrowRight, Code, Image, MessageSquare, Music, VideoIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const tools = [
+    {
+        label: 'Conversation',
+        icon: MessageSquare,
+        color: 'text-violet-500',
+        bgColor: 'bg-voilet-500/10',
+        href: '/conversation'
+    },
+    {
+        label: 'Music Generation',
+        icon: Music,
+        color: 'text-emerald-500',
+        bgColor: 'bg-emerald-500/10',
+        href: '/music'
+    },
+    {
+        label: 'Image Generation',
+        icon: Image,
+        color: 'text-pink-700',
+        bgColor: 'bg-pink-700/10',
+        href: '/image'
+    },
+    {
+        label: 'Video Generation',
+        icon: VideoIcon,
+        color: 'text-orange-700',
+        bgColor: 'bg-orange-700/10',
+        href: '/video'
+    },
+    {
+        label: 'Code Generation',
+        icon: Code,
+        color: 'text-green-500',
+        bgColor: 'bg-green-500/10',
+        href: '/code'
+    },
+]
+
+export default function DashboardPage() {
+    const router = useRouter();
+    return (
+        <div>
+            <div className="mb-8 space-y-4">
+                <h2 className="text-2xl font-bold text-center md:text-4xl">
+                    Explore the power of AI
+                </h2>
+                <p className="text-sm font-light text-center text-muted-foreground md:text-lg">
+                    Chat with the smartest AI - Expreience the utility of AI
+                </p>
+            </div>
+            <div className="px-4 space-y-4 md:px-20 lg:px-32">
+                {tools.map((tool) => (
+                    <Card
+                        onClick={() => router.push(`${tool.href}`)}
+                        key={tool.href}
+                        className="flex items-center justify-between p-4 transition cursor-pointer border-black/5 hover:shadow-md">
+                        <div className="flex items-center gap-x-4">
+                            <div className={cn('p-2 w-fit rounded-md', tool.bgColor)}>
+                                <tool.icon className={cn('w-8 h-8', tool.color)} />
+                            </div>
+                            <div className="font-semibold">
+                                {tool.label}
+                            </div>
+                        </div>
+
+                        <ArrowRight className="w-5 h-5" />
+                    </Card>
+                ))}
+            </div>
+        </div>
+    )
+}
---
+++ components/heading.tsx
@@ -0,0 +1,39 @@
+import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
+
+interface HeadingProps {
+    title: string;
+    description: string;
+    icon: LucideIcon;
+    iconColor?: string;
+    bgColor?: string;
+}
+
+const Heading: React.FC<HeadingProps> = ({
+    title,
+    description,
+    icon: Icon,
+    iconColor,
+    bgColor
+}) => {
+    return (
+
+        <div className="flex items-center px-4 mb-8 lg:px-8 gap-x-3">
+            <div className={cn('p-2 w-fit rounded-md', iconColor)}>
+                <Icon className={cn('w-10 h-10', iconColor)} />
+            </div>
+            <div>
+                <h2 className="text-3xl font-bold">
+                    {title}
+                </h2>
+                <p className="text-sm text-muted-foreground">
+                    {description}
+                </p>
+            </div>
+        </div>
+
+
+    );
+}
+
+export default Heading;
